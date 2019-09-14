2c3cce2c48ece84a6a0267aabd2b8fdd1602e7dc
masterdust12 <zbv1106@yahoo.com>
2019-09-14 11:57:45 -0700

    Phase 2 complete: By completely changing all of my networking code, the network now works between clients on the same computer (because localhost is being used rather than an actual ip.
    Adding player movement and everything should not be very difficult tbh.

--- src/com/Game/Init/Server.java
+++ src/com/Game/Init/Server.java
@@ -1,66 +1,101 @@
 package com.Game.Init;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.SocketException;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Server {
-    private static ObjectInputStream input;
-    private static ObjectOutputStream output;
-    private static ServerSocket ss;
-    private static Socket connection;
+    private int port;
+    private Thread listenThread;
+    private boolean listening = false;
+    private DatagramSocket socket;
+    private Set<PlayerConnection> connections;
+    private final int MAX_PACKET_SIZE = 1024;
+    private byte[] dataBuffer = new byte[MAX_PACKET_SIZE * 10];
 
-    public static void main(String[] args) throws IOException {
-        Server server = new Server();
-
-        server.startRunning();
+    public Server(int port) {
+        this.port = port;
+        connections = new HashSet<PlayerConnection>();
     }
 
-    public void startRunning() {
+    public void start() {
         try {
-            ss = new ServerSocket(3000, 5);
-            while (true) {
-                try {
-                    connect();
-                    setupConnection();
-                    detectPackets();
-                } catch (EOFException e) {
-                    System.out.println("Player has connected...");
-                } catch (SocketException e) {
-                    System.out.println("Player has disconnected...");
-                } finally {
-                    input.close();
-                    output.close();
-                    connection.close();
-                }
+            socket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            e.printStackTrace();
+        }
+
+        listening = true;
+
+        listenThread = new Thread(() -> listen());
+        listenThread.start();
+    }
+
+    private void listen() {
+        while (listening) {
+            DatagramPacket packet = new DatagramPacket(dataBuffer, dataBuffer.length);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
+            process(packet);
+        }
+    }
+
+    private void process(DatagramPacket packet) {
+        //dumpPacket(packet);
+
+        dataBuffer = new byte[MAX_PACKET_SIZE * 10];
+        String contents = new String(packet.getData());
+
+        String code = contents.substring(0, 2);
+        String message = contents.substring(2);
+
+        System.out.println(code);
+        System.out.println(contents);
+
+        switch (code) {
+            case "69": // connection code
+                // Do connection stuff
+                PlayerConnection connection = new PlayerConnection(packet.getAddress(), packet.getPort());
+                connections.add(connection);
+                break;
+            case "13": // Chat box message code
+                System.out.println(connections.size());
+                for (PlayerConnection c : connections) {
+                    System.out.println(c);
+                    send(contents.getBytes(), c.getIpAddress(), c.getPort());
+                }
+                break;
+        }
+
+        // Use a prefix to indicate the packet type, reference data from there
+    }
+
+    public void send(byte[] data, InetAddress address, int port) {
+        assert(socket.isConnected());
+        DatagramPacket packet = new DatagramPacket(data, data.length, address, port);
+        try {
+            socket.send(packet);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void connect() throws IOException {
-        connection = ss.accept();
-    }
+    private void dumpPacket(DatagramPacket packet) {
+        byte[] data = packet.getData();
+        InetAddress address = packet.getAddress();
+        int port = packet.getPort();
 
-    public void setupConnection() throws IOException {
-        output = new ObjectOutputStream(connection.getOutputStream());
-        output.flush();
-
-        input = new ObjectInputStream(connection.getInputStream());
-    }
-
-    public void detectPackets() throws IOException {
-        while (true) {
-            try {
-                String message = (String) input.readObject();
-                output.writeObject(message);
-                output.flush();
-                System.out.println(message);
-            } catch (ClassNotFoundException e) {
-                System.err.println("This user has sent an unknown object!");
-            }
-        }
+        System.out.println(".......................");
+        System.out.println("PACKET: ");
+        System.out.println("\t" + address.getHostAddress() + ":" + port);
+        System.out.println();
+        System.out.println("\tContents: " +new String(data).trim());
+        System.out.println(".......................");
     }
 }
